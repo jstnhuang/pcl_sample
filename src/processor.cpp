83c3f512b6427f3c722b46eaf97ee8e09f501d2f
Justin Huang <jstn@cs.washington.edu>
2014-10-13 19:56:18 -0700

    Added timer.

--- src/processor.cpp
+++ src/processor.cpp
@@ -4,20 +4,26 @@
 // See processor_node.cpp for explanations of what each header provides.
 #include "pcl_sample/processor.h"
 
-#include "pcl/point_types.h"
-#include "pcl_ros/point_cloud.h"
+#include "pcl/point_types.h"                 // pcl::PointXYZRGB
+#include "pcl_conversions/pcl_conversions.h" // pcl::fromROSMsg
+#include "pcl_ros/point_cloud.h"             // pcl::PointCloud
+#include "ros/console.h"                     // ROS_INFO
+#include "sensor_msgs/PointCloud2.h"         // sensor_msgs::PointCloud2
 
-#include <math.h>
+#include <math.h> // is_nan
 
 namespace pcl_sample {
-void Processor::Average(const pcl::PointCloud<pcl::PointXYZRGB> cloud,
+Processor::Processor(const ros::Time& prev_time)
+  : prev_time_(prev_time),
+    time_taken_(ros::Duration(0)),
+    num_calls_(0) {
+}
+
+void Processor::Average(const pcl::PointCloud<pcl::PointXYZRGB>& cloud,
     pcl::PointXYZRGB* average) { 
   double average_x = 0;
   double average_y = 0;
   double average_z = 0;
-  long average_r = 0;
-  long average_g = 0;
-  long average_b = 0;
   for (const pcl::PointXYZRGB& point : cloud.points) {
     // Some points may have NaN values for position.
     if (isnan(point.x) || isnan(point.y) || isnan(point.z)) {
@@ -26,15 +32,29 @@ void Processor::Average(const pcl::PointCloud<pcl::PointXYZRGB> cloud,
     average_x += point.x;
     average_y += point.y;
     average_z += point.z;
-    average_r += point.r;
-    average_g += point.g;
-    average_b += point.b;
   }
   average->x = average_x / cloud.points.size();
   average->y = average_y / cloud.points.size();
   average->z = average_z / cloud.points.size();
-  average->r = (double) average_r / cloud.points.size();
-  average->g = (double) average_g / cloud.points.size();
-  average->b = (double) average_b / cloud.points.size();
+  
+  ros::Time current_time = ros::Time::now();
+  time_taken_ += current_time - prev_time_;
+  prev_time_ = current_time;
+  num_calls_ += 1;
 }
+
+void Processor::Callback(const sensor_msgs::PointCloud2ConstPtr& msg) {
+  // Use PCL's conversion.
+  pcl::PointCloud<pcl::PointXYZRGB> cloud;
+  pcl::fromROSMsg(*msg, cloud);
+  pcl::PointXYZRGB average;
+
+  Average(cloud, &average);
+
+  // Log the average point at the INFO level. You can specify other logging
+  // levels using ROS_DEBUG, ROS_WARN, etc. Search for "ros logging" online.
+  ROS_INFO("x: %f y: %f z: %f, time/point: %fs",
+    average.x, average.y, average.z, time_taken_.toSec() / num_calls_);
+}
+
 }
--- src/processor_node.cpp
+++ src/processor_node.cpp
@@ -4,54 +4,32 @@
 //   rosrun pcl_sample processor_node pointcloud2_in:=/camera/depth_registered/points
 //
 // Note: the PointCloud2 data from the lab Turtlebots uses a different
-// coordinate system from the ROS convention. We will discuss this more later.
+// coordinate system from the ROS convention. See the README on Github for more
+// information.
 
-// Technically, console.h, node_handle.h, and subscriber.h are transitively
-// included with ros.h. However, it's generally a good C++ practice to always
-// "include what you use" (IWYU).
-#include "pcl/point_types.h"                 // PointXYZRGB
-#include "pcl_conversions/pcl_conversions.h" // pcl::fromROSMsg
-#include "pcl_ros/point_cloud.h"             // pcl::PointCloud
-#include "pcl_sample/processor.h"            // pcl_sample::Processor
-#include "ros/console.h"                     // ROS_INFO
-#include "ros/ros.h"                         // ros::init, ros::spin, etc.
-#include "ros/node_handle.h"                 // ros::NodeHandle
-#include "ros/subscriber.h"                  // ros::Subscriber
-#include "sensor_msgs/PointCloud2.h"         // sensor_msgs::PointCloud2
+// Technically, node_handle.h, and subscriber.h are transitively included with
+// ros.h. However, it's generally a good C++ practice to always "include what
+// you use" (IWYU).
+#include "pcl_sample/processor.h" // pcl_sample::Processor
+#include "ros/ros.h"              // ros::init, ros::spin, etc.
+#include "ros/node_handle.h"      // ros::NodeHandle
+#include "ros/subscriber.h"       // ros::Subscriber
 
 using pcl_sample::Processor;
-
-// Callback for when a PointCloud2 message is received.
-void callback(const sensor_msgs::PointCloud2ConstPtr& msg) {
-  // Use PCL's conversion.
-  pcl::PointCloud<pcl::PointXYZRGB> cloud;
-  pcl::fromROSMsg(*msg, cloud);
-  pcl::PointXYZRGB average;
-
-  // Call our library function. We're lucky it's a static method, since we want
-  // to call it inside of our callback. If our processor class had some state,
-  // then we'd need to make this callback a method in a new class, e.g.,
-  // ProcessorNode. Or, this function could be a functor.
-  // See http://wiki.ros.org/roscpp/Overview/Publishers%20and%20Subscribers#Callback_Types
-  Processor::Average(cloud, &average);
-
-  // Log the average point at the INFO level. You can specify other logging
-  // levels using ROS_DEBUG, ROS_WARN, etc. Search for "ros logging" online.
-  ROS_INFO("x: %f y: %f z: %f, r: %d g: %d b: %d",
-    average.x, average.y, average.z,
-    average.r, average.g, average.b);
-}
 
 int main(int argc, char** argv) {
   // Initialize this program as a ROS node.
   ros::init(argc, argv, "sub_pcl");
   ros::NodeHandle nh;
+  Processor processor(ros::Time::now());
 
   // Subscribe to /pointcloud2_in. Normally this would be remapped to another
   // topic, such as /camera/depth_registered/points. Calls the callback
   // function when a point cloud comes in.
+  // Read more about the callback syntax at:
+  // http://wiki.ros.org/roscpp/Overview/Publishers%20and%20Subscribers#Callback_Types
   ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>(
-    "pointcloud2_in", 1, callback);
+    "pointcloud2_in", 1, &Processor::Callback, &processor);
   ros::spin();
 }
 
